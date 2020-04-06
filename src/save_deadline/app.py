2b697cc8564a468030b0f1a4fc6d396dc30d5367
cm-fujii-genki <fujii.genki@classmethod.jp>
2020-04-06 19:12:11 +0900

    api verify

--- src/save_deadline/app.py
+++ src/save_deadline/app.py
@@ -1,9 +1,17 @@
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    logger.info(json.dumps(event))
+
+    body = json.loads(event['body'])
+
     return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {"test": "message"},
+        'statusCode': 200,
+        'body': json.dumps(
+            {'challenge': body['challenge']},
         ),
     }
