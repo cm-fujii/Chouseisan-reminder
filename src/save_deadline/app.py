fc0cbd0081f44c0815547617baaaa08d8f3e8415
cm-fujii-genki <fujii.genki@classmethod.jp>
2020-04-07 10:30:29 +0900

    parse body text

--- src/save_deadline/app.py
+++ src/save_deadline/app.py
@@ -1,15 +1,48 @@
 import json
 import logging
+import re
+
+from datetime import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    main(event)
+    return {
+        'statusCode': 200
+    }
+
+def main(event):
     logger.info(json.dumps(event))
 
     body = json.loads(event['body'])
     logger.info(json.dumps(body))
 
-    return {
-        'statusCode': 200
-    }
+    if 'username' not in body['event']:
+        logger.info('No username.')
+        return
+
+    if 'reminder_misc_join_201901_workflow' != body['event']['username']:
+        logger.info('No workflow message.')
+        return
+
+    deadline = parse_timestamp(body['event']['text'])
+    url = parse_url(body['event']['text'])
+    logger.info(f'deadline: {deadline}, url: {url}')
+
+
+def parse_timestamp(text):
+    pattern = r'.+\n期限は \*(\d{4}/\d{1,2}/\d{1,2})\* です！'
+    res = re.match(pattern, text)
+    if res:
+        return datetime.strptime(res.group(1), '%Y/%m/%d').timestamp()
+    raise ValueError
+
+
+def parse_url(text):
+    pattern = r'.+\n.+\n<(.+)>'
+    res = re.match(pattern, text)
+    if res:
+        return int(res.group(1))
+    raise ValueError
---
+++ tests/unit/test_handler_save_deadline.py
@@ -0,0 +1,36 @@
+import json
+
+import pytest
+
+from datetime import date
+from src.save_deadline import app
+
+@pytest.mark.parametrize(
+    'text, expected', [
+        (
+            '調整さんに記入をお願いします！\n期限は *2020/04/06* です！\n<https://chouseisan.com/s?h=xxx>',
+            1586098800
+        ),
+        (
+            '調整さんに記入をお願いします！\n期限は *2020/5/9* です！\n<https://chouseisan.com/s?h=xxx>',
+            1588950000
+        ),
+        (
+            '調整さんに記入をお願いします！\n期限は *2020/12/12* です！\n<https://chouseisan.com/s?h=xxx>',
+            1607698800
+        ),
+    ])
+def test_parse_timestamp(text, expected):
+    actual = app.parse_timestamp(text)
+    assert expected == actual
+
+@pytest.mark.parametrize(
+    'text, expected', [
+        (
+            '調整さんに記入をお願いします！\n期限は *2020/04/06* です！\n<https://chouseisan.com/s?h=xxx>',
+            'https://chouseisan.com/s?h=xxx'
+        ),
+    ])
+def test_parse_url(text, expected):
+    actual = app.parse_url(text)
+    assert expected == actual
